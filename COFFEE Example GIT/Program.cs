78bd416279f6d6de2d8973a2f0c15f088018ee98
Tristan Robson <T.r.i.corp@outlook.com>
2020-04-17 11:25:12 +0100

    Stage change 1

---
+++ COFFEE Example GIT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace COFFEE_Example_GIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 123!");
+            Console.WriteLine("Change 1");
+
+        }
+        static void sub(string[] args)
+        {
+            Console.WriteLine("Change 2");
+        }
+    }
+
+}
